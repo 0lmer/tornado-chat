3b3f6265d672afea0b264e6c347aa3028cb69bf8
Dmitry <dv.karnaukh@gmail.com>
2015-05-08 23:16:28 +0300

    fixed insert

--- server.py
+++ server.py
@@ -6,15 +6,21 @@ import tornado.web
 import tornado.ioloop
 import tornado.websocket
 from tornado import template
-import pymongo
-# import asyncmongo
+# import pymongo
+import asyncmongo
 
 
 class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
     def get(self):
         db = self.application.db
-        messages = db.chat.find()
-        self.render('index.html', messages=messages)
+        db.chat.find(callback=self._on_response)
+
+    def _on_response(self, response, error):
+        if error:
+            raise tornado.web.HTTPError(500)
+        self.render('index.html', messages=response)
+
 
 class WebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
@@ -22,16 +28,22 @@ class WebSocket(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         db = self.application.db
-        message_dict = json.loads(message);
-        db.chat.insert(message_dict)
-        for key, value in enumerate(self.application.webSocketsPool):
-            if value != self:
-                value.ws_connection.write_message(message)
+        message_dict = json.loads(message)
+
+        def _send_to_another_people(response, error):
+            if error:
+                return
+            for key, value in enumerate(self.application.webSocketsPool):
+                if value != self:
+                    value.ws_connection.write_message(message)
+
+        db.chat.insert(message_dict, callback=_send_to_another_people)
 
     def on_close(self, message=None):
         for key, value in enumerate(self.application.webSocketsPool):
             if value == self:
                 del self.application.webSocketsPool[key]
+
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -40,8 +52,6 @@ class Application(tornado.web.Application):
         settings = {
             'static_url_prefix': '/static/',
         }
-        connection = pymongo.Connection('127.0.0.1', 27017)
-        self.db = connection.chat
         handlers = (
             (r'/', MainHandler),
             (r'/websocket/?', WebSocket),
@@ -50,6 +60,13 @@ class Application(tornado.web.Application):
         )
 
         tornado.web.Application.__init__(self, handlers)
+
+    @property
+    def db(self):
+        if not hasattr(self, '_db'):
+            self._db = asyncmongo.Client(pool_id='chat', host='127.0.0.1', port=27017, maxcached=10,
+                                             maxconnections=50, dbname='chat')
+        return self._db
 
 application = Application()
 
